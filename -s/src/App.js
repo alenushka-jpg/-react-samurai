110b4dbbbe2f612e2bf99f3108d715ece6686df9
alenushka <alionxlo@gmail.com>
2022-03-20 08:26:52 +0300

    step 1, writes html code and grids

--- -s/src/App.js
+++ -s/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-container'>
+      <header className='app-container__header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtnqJC_pbyrULA6yp3ifRGkCRijOXoXDf-qA&usqp=CAU'></img>
       </header>
+      <nav className='app-container__navigation'>
+        <ul>
+          <li>
+            <a>Profile</a>
+          </li>
+          <li>
+            <a>Messages</a>
+          </li>
+          <li>
+            <a>News</a>
+          </li>
+          <li>
+            <a>Music</a>
+          </li>
+        </ul>
+      </nav>
+      <div className='app-container__section'>
+        <img src='https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg'></img>
+        <section className='app-container__content'>ava + description</section>
+        <section className='app-container__my-post'>
+          my post
+          <div className='app-container__new-post'>new post</div>
+        </section>
+        <section className='app-container__post'>post 1</section>
+        <section className='app-container__post'>post 2</section>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
