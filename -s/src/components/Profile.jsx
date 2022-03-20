4a44ac208c42475ce535b0c499a7a4559066bb16
alenushka <alionxlo@gmail.com>
2022-03-20 12:08:32 +0300

    step 4, adds styles

--- -s/src/components/Profile.jsx
+++ -s/src/components/Profile.jsx
@@ -5,7 +5,18 @@ const Profile = () => {
   return (
     <div className={classes.profile}>
       <img src='https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg'></img>
-      <section className={classes.content}>ava + description</section>
+      <section className={classes.content}>
+        <div className={classes.contentImg}>
+          <img src="https://avatarko.ru/img/kartinka/13/zhivotnye_sobaka_ochki_12359.jpg"/>
+        </div>
+        <div className={classes.contentDescription}>
+          <p>Brutal dog</p>
+          <span>Date of Bird: 19 january</span>
+          <span>City: Tel-a-viv</span>
+          <span>Education: frontend-development</span>
+          <span>Web Site: *in developing*</span>
+        </div>
+      </section>
       <section className={classes.myPost}>
         <div className={classes.newPost}>
           <h2>new post</h2>
