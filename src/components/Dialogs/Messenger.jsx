0c848656b9473462d5ca0d44bda31daf811f9884
alenushka <alionxlo@gmail.com>
2022-03-30 20:23:14 +0300

    step 10, pushes data aside

--- src/components/Dialogs/Messenger.jsx
+++ src/components/Dialogs/Messenger.jsx
@@ -15,21 +15,36 @@ const MessageItem = (props) => {
 }
 
 const Messenger = () => {
+  let messengerData = [
+    {id: 1, name: 'Axel Doberman'},
+    {id: 2, name: 'Cooper labrador'},
+    {id: 3, name: 'Spike Pitbull'},
+    {id: 4, name: 'Arnold Rottweiler'},
+    {id: 5, name: 'Connor Spitz'}
+  ];
+
+  let messageUsers = [
+    {id: 1, message: 'Hello, dog on the line, how do you hear? I repeat, how do you hear?'},
+    {id: 2, message: 'Hey, how"s the cat you shot?'},
+    {id: 3, message: 'Brutal dog meeting tonight at 8pm'},
+    {id: 4, message: 'My master is crazy! He puts on my collar and gets on all fours!'},
+    {id: 5, message: 'Have you been castrated yet?'}
+  ]
   return (
     <section className={classes.messenger}>
       <div className={classes.messenger__users}>
-        <UserMessange name='Axel Doberman' id='1' />
-        <UserMessange name='Cooper labrador' id='2' />
-        <UserMessange name='Spike Pitbull' id='3' />
-        <UserMessange name='Arnold Rottweiler' id='4' />
-        <UserMessange name='Connor Spitz' id='5' />
+        <UserMessange name={messengerData[0].name} id={messengerData[0].id} />
+        <UserMessange name={messengerData[1].name} id={messengerData[1].id} />
+        <UserMessange name={messengerData[2].name} id={messengerData[2].id} />
+        <UserMessange name={messengerData[3].name} id={messengerData[3].id} />
+        <UserMessange name={messengerData[4].name} id={messengerData[4].id} />
       </div>
       <div className={classes.messenger__item}>
-        <MessageItem message='Hello, dog on the line, how do you hear? I repeat, how do you hear?' />
-        <MessageItem message='Hey, how"s the cat you shot?' />
-        <MessageItem message='Brutal dog meeting tonight at 8pm' />
-        <MessageItem message='My master is crazy! He puts on my collar and gets on all fours!' />
-        <MessageItem message='Have you been castrated yet?' />
+        <MessageItem message={messageUsers[0].message} />
+        <MessageItem message={messageUsers[1].message} />
+        <MessageItem message={messageUsers[2].message} />
+        <MessageItem message={messageUsers[3].message} />
+        <MessageItem message={messageUsers[4].message} />
       </div>
     </section>
   )
